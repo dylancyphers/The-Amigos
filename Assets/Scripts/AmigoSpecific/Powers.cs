c86cf52ade88b8c49c336e12d680490647818e12
rburnwo <TeamGold1819@gmail.com>
2019-05-07 21:16:28 -0500

    added power.cs

---
+++ Assets/Scripts/AmigoSpecific/Powers.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powers {
+
+    public static bool doubleJump;
+    public static bool wallJump;
+    public static bool sprint;
+    public static bool crouch;
+}
