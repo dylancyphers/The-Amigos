3140cb2268030cd86ac501b413d8db33ff6cb83e
Jonathan Reynolds <jack.reynolds96@gmail.com>
2019-05-07 23:58:58 -0500

    Merge branch 'dev' of https://github.com/dylancyphers/The-Amigos into dev

---
+++ Assets/Scripts/AmigoSpecific/Powers.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powers : MonoBehaviour {
+
+    public static bool doubleJump;
+    public static bool wallJump;
+    public static bool sprint;
+    public static bool crouch;
+    // Use this for initialization
+    void Start () {
+
+        doubleJump = false;
+        wallJump = false;
+        sprint = false;
+        crouch = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public static void FoundDoubleJump()
+    {
+        doubleJump = true;
+    }
+    public static void FoundWallJump()
+    {
+        wallJump = true;
+    }
+    public static void FoundSprint()
+    {
+        sprint = true;
+    }
+    public static void FoundCrouch()
+    {
+        crouch = true;
+    }
+
+
+
+}
